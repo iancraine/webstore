494964c53dad9273b7fbf8c5f079257100b6e114
Ian <iancraine09@gmail.com>
2024-02-06 01:25:24 -0600

    cart page added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { CartComponent } from './components/cart/cart.component';
 
 const routes: Routes = [{
   path: 'home',
   component: HomeComponent
+},
+{
+  path: 'cart',
+  component: CartComponent
 },
 {path: '', redirectTo: 'home', pathMatch: 'full'},
 ];
