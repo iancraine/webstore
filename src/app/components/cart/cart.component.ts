032950aacdcfeb1d55dae49c4f1795aac72df6d4
Ian <iancraine09@gmail.com>
2024-02-09 01:58:12 -0600

    Cart Logic started

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Cart, CartItem } from 'src/app/models/cart.model';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
   cart : Cart = {items: [{
     product: 'https://via.placeholder.com/150',
     name: 'snickers',
@@ -32,16 +33,22 @@ export class CartComponent {
     'action'
   ];
 
-  constructor(){
+  constructor(private cartService: CartService){
 
   }
   ngOnInit(){
-    this.dataSource = this.cart.items;
+    
+    this.cartService.cart.subscribe((_cart: Cart) => {
+      this.cart = _cart;
+      this.dataSource = this.cart.items;
+    })
   }
 
   getTotal(items: Array<CartItem>): number{
-    return items.
-      map((item)=> item.price * item.quantity)
-      .reduce((prev, current) =>prev + current,0);
+    return this.cartService.getTotal(items);
+  }
+
+  onClearCart(): void{
+    this.cartService.clearCart();
   }
 }
--- src/app/components/product-box/product-box.component.ts
+++ src/app/components/product-box/product-box.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
 
 @Component({
   selector: 'app-product-box',
@@ -6,5 +7,18 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./product-box.component.css']
 })
 export class ProductBoxComponent {
-@Input() fullWidthMode = false;
+  @Input() fullWidthMode = false;
+  product: Product| undefined = {
+    id: 1,
+    title: 'Snickers',
+    price: 150,
+    category: 'shoes',
+    description: 'description',
+    image: 'https://via.placeholder.com/150',
+  };
+  @Output() addToCart = new EventEmitter();
+
+  onAddToCart(){
+    this.addToCart.emit(this.product);
+  }
 }
